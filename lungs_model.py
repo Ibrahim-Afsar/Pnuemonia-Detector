e598fd285fc7ec0eb2c6408fb02b4c7f613bab9c
Ibrahim Afsar <156586620+Ibrahim-Afsar@users.noreply.github.com>
2024-12-30 18:39:06 +0500

    added pneumonia detector model

---
+++ lungs_model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+train_dir = 'D:\Machine learning Datasets\Lungs Pneumonia dataset/archive/xray_dataset_covid19/train'
+test_dir = 'D:\Machine learning Datasets\Lungs Pneumonia dataset/archive/xray_dataset_covid19/test'
+
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(224, 224), batch_size=32, class_mode='binary')
+test_generator = test_datagen.flow_from_directory(test_dir, target_size=(224, 224), batch_size=32, class_mode='binary')
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(224, 224, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(train_generator, steps_per_epoch=len(train_generator), epochs=15, validation_data=test_generator, validation_steps=len(test_generator))
